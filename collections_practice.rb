cfc298795433c2bb3042ab0dd023e45199c9d423
Dylan Michael Labat <dml252@live.com>
2019-01-14 01:30:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |word|
+    word.start_with?("r")
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word[0..1] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |item|
+    item.class != String
+  end
+end
+
+def count_elements(array)
+  hash = Hash.new(0)
+  array.each do |name|
+    hash[name] += 1
+  end
+  hash
+end
